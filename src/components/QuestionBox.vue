bf8c7f48a88233d4783ea57e1f913592fdc4e0db
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-09 14:22:11 -0700

    Add methods

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,7 +2,12 @@
   <div class='question-box-container'>
     <div class='question-container'>
       <h3>{{ currentQuestion.question }}</h3>
-      <p v-for='answer in answers' :key='answer' class='possible-answer'>
+      <p v-for='(answer, index) in answers'
+        :key='answer'
+        class='possible-answer'
+        @click.prevent='selectAnswer(index)'
+        :class="[ selectedIndex === index ? 'selected' : '' ]"
+      >
         {{ answer }}
       </p>
       <div class='buttons-container'>
@@ -14,10 +19,32 @@
 </template>
 
 <script>
+  import _ from 'lodash';
   export default {
     props: {
       currentQuestion: Object,
       next: Function
+    },
+    methods: {
+      selectAnswer(index) {
+        this.selectedIndex = index
+      },
+      shuffleAnswers() {
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
+      }
+    },
+    watch: {
+      currentQuestion() {
+        this.selectedIndex = null
+        this.shuffleAnswers()
+      }
+    },
+    data() {
+      return {
+        selectedIndex: null,
+        shuffledAnswers: []
+      }
     },
     computed: {
       answers() {
