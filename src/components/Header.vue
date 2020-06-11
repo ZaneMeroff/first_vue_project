d511364b4ed12b1ffe9c1d6ed10f346bc2304e1b
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-11 13:59:31 -0600

    Add props

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,15 @@
 <template>
   <div class='header-container'>
     <h1 class='title'>🐛 ANIMAL QUIZ 🤨</h1>
-    <p class='question-counter'>question counter: 1/10</p>
+    <p class='question-counter'>correct answers: {{numCorrect}}/{{numTotal}}</p>
   </div>
 </template>
+
+<script>
+  export default {
+    props: [
+      'numCorrect',
+      'numTotal'
+    ]
+  }
+</script>
