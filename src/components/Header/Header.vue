21a5dffd9ea47202a40e84dbfeac579403c8ebe0
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-11 15:48:00 -0600

    Relocate file

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='header-container'>
+    <h1 class='title'>🐛 ANIMAL QUIZ 🤨</h1>
+    <p class='question-counter'>correct answers: {{ numCorrect }}/{{ numTotal }}</p>
+    <p class='question-counter'>round number: {{ roundNumber + 1 }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'numCorrect',
+      'numTotal',
+      'roundNumber'
+    ]
+  }
+</script>
