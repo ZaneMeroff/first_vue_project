b8c678dbcd5b1909edfec232fffb71807f7c22c0
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-11 22:17:26 -0600

    Bring in prop

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,6 +3,7 @@
     <h1 class='title'>animal trivia</h1>
     <p class='header-text'>correct answers: {{ numCorrect }}/{{ numTotal }}</p>
     <p class='header-text'>round number: {{ roundNumber + 1 }}</p>
+    <button @click='onRulesButtonClick' class='view-rules-button'>{{ !rulesVisible  ? 'how to play' : 'back to game' }}</button>
   </div>
 </template>
 
@@ -11,7 +12,14 @@
     props: [
       'numCorrect',
       'numTotal',
-      'roundNumber'
-    ]
+      'roundNumber',
+      'rulesVisible',
+      'handleRulesVisible'
+    ],
+    methods: {
+      onRulesButtonClick() {
+        this.handleRulesVisible()
+      }
+    }
   }
 </script>
